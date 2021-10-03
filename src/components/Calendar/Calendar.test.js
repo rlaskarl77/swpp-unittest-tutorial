4ace58726d56950ef64a4caae9dfebfe3d81e726
김남기 <kimnamgi@Kimnamgi-MacBookAir.local>
2021-10-03 16:44:05 +0900

    todocalendar and calendar test added

--- src/components/Calendar/Calendar.test.js
+++ src/components/Calendar/Calendar.test.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from 'semantic-ui-react'
+import { Table } from 'semantic-ui-react';
 import Calendar from "./Calendar";
 import { shallow } from "enzyme";
 
@@ -12,7 +12,22 @@ const data = {
 
 describe('<Calendar />', () => {
     let calendar;
-    beforeEach(()=>{
-        d
+    // beforeEach(()=>{
+    //     ;
+    // });
+    
+    it('should be rendered properly', ()=>{
+        calendar = shallow(<Calendar />);
+        const wrapper = calendar.find('Table');
+        expect(wrapper.length).toBe(1);
+
+    });
+
+    it('should render body properly', () => {
+        calendar = shallow(<Calendar year={2021} month={10} clickDone={true} todos={[{year:2021, month:10, date:1, done:true, id:1}]}/>);
+        const wrapper = calendar.find('Table');
+        expect(wrapper.length).toBe(1);
+        const title = calendar.find('.date div');
+        expect(title.length).toBe(0);
     });
 });
--- src/containers/TodoCalendar/TodoCalendar.test.js
+++ src/containers/TodoCalendar/TodoCalendar.test.js
@@ -7,15 +7,65 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 import TodoCalendar from './TodoCalendar';
 import { getMockStore } from '../../test-utils/mocks';
 import { history } from '../../store/store';
-import * as actionCreators from '../../store/actions/todo';
+
+
+jest.mock('../../components/Calendar/Calendar', ()=>{
+    return jest.fn(props => {
+        return (
+            <div className="spyCalendar">
+            </div>
+        )
+    })
+})
+
+const stubInitialState = {
+    todos:[{id:1, year:2021, month:10, done:true}],
+    selectedTodo: null
+};
+
+const mockStore = getMockStore(stubInitialState);
+
 
 
 describe('<TodoCalendar />', () => {
     let todoCalendar;
+
     beforeEach(()=>{
         todoCalendar = (
-            
-        )
+            <Provider store={mockStore}>
+                <ConnectedRouter history={history}>
+                <Switch>
+                    <Route path='/' render={()=> <TodoCalendar />}/>
+                </Switch>
+                </ ConnectedRouter>
+            </Provider>
+        );
+    })
+
+    it('should be rendered properly', ()=>{
+        const component = mount(todoCalendar);
+        const wrapper = component.find(".header");
+        expect(wrapper.length).toBe(1);
+    });
+   
+    it('should render calendar', () => {
+        const component = mount(todoCalendar);
+        const wrapper = component.find('.spyCalendar');
+        expect(wrapper.length).toBe(1);
+    });
+
+    it('should handle click', ()=>{
+        const component = mount(todoCalendar);
+        const buttons = component.find('.header button');
+        expect(buttons.length).toBe(2);
+        let newCalendarInstance = component.find(TodoCalendar.WrappedComponent).instance();
+        expect(newCalendarInstance.state.month).toEqual(9);
+        buttons.at(0).simulate('click');
+        newCalendarInstance = component.find(TodoCalendar.WrappedComponent).instance();
+        expect(newCalendarInstance.state.month).toEqual(8);
+        buttons.at(1).simulate('click');
+        newCalendarInstance = component.find(TodoCalendar.WrappedComponent).instance();
+        expect(newCalendarInstance.state.month).toEqual(9);
     })
 
 });
